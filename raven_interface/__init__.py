b9e0e53b23828bbbfc007251dd6b840f7ea23ed3
jts <jtswarthout@gmail.com>
2025-12-16 09:57:36 -0500

    Initial commit

---
+++ raven_interface/__init__.py
@@ -0,0 +1,192 @@
+from otree.api import *
+import json
+import csv
+
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'raven_interface'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 36
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    qid = models.StringField()
+    question = models.StringField()
+    alpha = models.FloatField()
+    num_tokens = models.IntegerField()
+    beta = models.FloatField()
+    color = models.StringField()
+    bin_labels = models.StringField()
+    response = models.StringField()
+    response_bin1 = models.FloatField(initial=-1)
+    response_bin2 = models.FloatField(initial=-1)
+    response_bin3 = models.FloatField(initial=-1)
+    response_bin4 = models.FloatField(initial=-1)
+    response_bin5 = models.FloatField(initial=-1)
+    response_bin6 = models.FloatField(initial=-1)
+    response_bin7 = models.FloatField(initial=-1)
+    response_bin8 = models.FloatField(initial=-1)
+    correct_bin = models.IntegerField(initial=-1)
+    earnings = models.FloatField(initial=0)
+    accuracy = models.FloatField()
+    efficiency = models.FloatField()
+
+
+# PAGES
+
+class Intro(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 1
+
+
+class Gender(Page):
+    form_model = 'player'
+    form_fields = ['gender']
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1
+
+
+class MyPage(Page):
+
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number <= player.session.vars['max_round']
+
+    form_model = 'player'
+    form_fields = ['response']
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        score_response(player)
+        # print(player.response)
+        # print(json.loads(player.response))
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        qid = player.qid
+
+        image_paths = []
+
+        # Add the base image file
+        image_paths.append(f'images/raven/{qid}.png')
+
+        # Add the 8 subfiles with suffixes _1 through _8
+        for i in range(1, 9):
+            relative_path = f'images/raven/{qid}_{i}.png'
+            image_paths.append(relative_path)
+
+        return dict(
+            image_paths=image_paths,
+        )
+
+
+class ResultsWaitPage(WaitPage):
+    pass
+
+
+class Results(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == player.session.vars['max_round']
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        s = f"""
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col" class="col-1 text-center">Question</th>
+                        <th scope="col" class="col-1 text-center">% tokens correctly allocated</th>
+                        <th scope="col" class="col-1 text-center">Earnings</th>
+                        <th scope="col" class="col-1 text-center">Earnings efficiency</th>
+                    </tr>
+                </thead>
+        """
+        sum_accuracy = 0
+        sum_earnings = 0
+        sum_efficiency = 0
+        for i in range(player.session.vars['max_round']):
+            p = player.in_round(i+1)
+            sum_accuracy += p.accuracy
+            sum_earnings += p.earnings
+            sum_efficiency += p.efficiency
+            s += "<tr>"
+            s += "    <td class='text-center'>" + str(p.round_number) + "</td>"
+            s += "    <td class='text-center'>" + str(round(p.accuracy*100, 2)) + "</td>"
+            s += "    <td class='text-center'>$" + str(round(p.earnings, 2)) + "</td>"
+            s += "    <td class='text-center'>" + str(round(p.efficiency, 4)) + "</td>"
+            s += "</tr>"
+        average_accuracy = sum_accuracy / player.session.vars['max_round']
+        average_earnings = sum_earnings / player.session.vars['max_round']
+        average_efficiency = sum_efficiency / player.session.vars['max_round']
+        s += "    <tr>"
+        s += "        <td class='text-center'> AVERAGE</td>"
+        s += "        <td class='text-center'>" + str(round(average_accuracy*100, 2)) + "</td>"
+        s += "        <td class='text-center'>$" + str(round(average_earnings, 2)) + "</td>"
+        s += "        <td class='text-center'>" + str(round(average_efficiency, 4)) + "</td>"
+        s += "</table>"
+
+        return dict(
+            my_table=s,
+        )
+# FUNCTIONS
+
+def creating_session(subsession: Subsession):
+    if subsession.round_number == 1:
+        subsession.session.vars['max_round'] = len(subsession.session.config['questions'])
+    if subsession.round_number <= subsession.session.vars['max_round']:
+        for p in subsession.get_players():
+            p.qid = subsession.session.config['questions'][subsession.round_number-1][0]
+            p.question = "Some text goes about here."
+            p.alpha = 1
+            p.beta = 1
+            p.num_tokens = 80
+            p.bin_labels = json.dumps(['1', '2', '3', '4', '5', '6', '7', '8', ])
+            p.color = json.dumps(['#6495ED'])
+
+
+def score_response(player: Player):
+    response = json.loads(player.response)
+    for i in range(len(response)):
+        response[i] = response[i] / player.num_tokens
+    print(response)
+    def ScoringRule(cb):
+        SS = 0.0
+        # Dim Result As Single
+        for i in range(8):
+            SS += response[i] * response[i]
+            print(i, ' SS: ', SS)
+        score = player.alpha + player.beta * ((2 * response[cb]) - SS)
+        return score
+
+    player.correct_bin = player.session.config['questions'][player.subsession.round_number-1][2] - 1
+    player.earnings = ScoringRule(player.correct_bin)
+
+    player.response_bin1 = response[0]
+    player.response_bin2 = response[1]
+    player.response_bin3 = response[2]
+    player.response_bin4 = response[3]
+    player.response_bin5 = response[4]
+    player.response_bin6 = response[5]
+    player.response_bin7 = response[6]
+    player.response_bin8 = response[7]
+
+    player.accuracy = response[player.correct_bin]
+    player.efficiency = player.earnings / (player.alpha + player.beta)
+
+
+page_sequence = [Intro, MyPage, Results]
---
+++ risk/__init__.py
@@ -0,0 +1,211 @@
+from otree.api import *
+import random
+import json
+import itertools
+import csv
+
+c = Currency
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'risk'
+    players_per_group = None
+    num_rounds = 60
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    qid = models.StringField()
+    left_lottery = models.StringField()
+    right_lottery = models.StringField()
+    response = models.StringField()
+    random_number = models.IntegerField()
+    random_coin = models.StringField()
+
+
+
+# FUNCTIONS
+
+def creating_session(subsession):
+
+    if subsession.round_number == 1:
+        print(subsession.session.vars)
+        params = subsession.session.config['risk_params']
+        sample_sizes = subsession.session.config['risk_sample_sizes']
+        subsession.session.vars['risk_last_round'] = sum(sample_sizes)
+        # n = subsession.session.vars['risk_last_round']
+
+    # blocks = params[0]
+
+    for p in subsession.get_players():
+        if subsession.round_number == 1:
+            p.participant.vars['selected_risk_questions'] = []
+
+            for b in params:
+                print('b', b)
+                print('b index', params.index(b))
+                print('sample_sizes', sample_sizes)
+                print('a', p.participant.vars['selected_risk_questions'])
+                p.participant.vars['selected_risk_questions'].append(random.sample(b, sample_sizes[params.index(b)]))
+                print('b', p.participant.vars['selected_risk_questions'])
+            # Eventually we want additional options here that would allow for presenting questions in block sequence,
+            # and perhaps shuffling block order. For now, we will simply randomize questions across blocks.
+
+            # IMPORTANT: questions are currently still in separate block sublists. We must extract them from the sublists
+            # so that they are all in a top level without being grouped by block. Then we can shuffle this top level list.
+
+            p.participant.vars['selected_risk_questions'] = list(itertools.chain.from_iterable(p.participant.vars['selected_risk_questions']))
+
+            random.shuffle(p.participant.vars['selected_risk_questions'])
+
+        if subsession.round_number <= subsession.session.vars['risk_last_round']:
+            # question_number = p.participant.vars['risk_sequence'][subsession.round_number - 1]
+            # print('r', subsession.round_number)
+            # print('length of pvars_r_questions', len(p.participant.vars['selected_risk_questions']))
+            question = p.participant.vars['selected_risk_questions'][subsession.round_number - 1]
+            # print('pvars_selected_risk_questions', p.participant.vars['selected_risk_questions'])
+            # print('q:', question)
+            p.qid = question[0]
+            p.left_lottery = json.dumps(question[1])
+            p.right_lottery = json.dumps(question[2])
+            p.random_number = random.randint(1, 100)
+            p.random_coin = random.choice(['D', 'N'])
+
+
+def check_partition(random_int, original_partition):
+    """
+    Checks which part of the original partition a random integer falls into.
+
+    Args:
+      random_int: A random integer between 1 and the total length of the expanded partition (inclusive).
+      original_partition: A list representing the partition of the probability space.
+                          Each element in the list represents the size of a segment
+                          in the expanded partition.
+
+    Returns:
+      The index of the original partition the random integer corresponds to.
+      Returns None if the input is outside the valid range.
+    """
+    total_length = sum(original_partition)
+    if not 1 <= random_int <= total_length:
+        return None
+
+    expanded_partition = []
+    for i, size in enumerate(original_partition):
+        expanded_partition.extend([i] * size)
+
+    return expanded_partition[random_int - 1]
+
+
+#
+# PAGES
+#
+class Instructions(Page):
+    def is_displayed(player):
+        return player.subsession.round_number == 1
+
+    def vars_for_template(player):
+        return dict(
+
+        )
+
+
+class MyPage(Page):
+    form_model = 'player'
+    form_fields = ['response']
+
+    def vars_for_template(player):
+        return dict(
+            qid=player.qid,
+            left_lottery=json.loads(player.left_lottery),
+            right_lottery=json.loads(player.right_lottery),
+        )
+
+    def is_displayed(player):
+        return player.subsession.round_number <= player.subsession.session.vars['risk_last_round']
+
+    def before_next_page(player: Player, timeout_happened):
+        # After participant makes all choices, select a round for payment and determine payoff.
+        if player.round_number == player.subsession.session.vars['risk_last_round']:
+
+            lottery = None
+
+            # Randomly select a prior round and save the associated player instance
+            p_prior = random.choice(player.in_all_rounds())
+
+            if p_prior.response == 'L':
+                lottery = json.loads(p_prior.left_lottery)
+            elif p_prior.response == 'R':
+                lottery = json.loads(p_prior.right_lottery)
+            outcome = check_partition(p_prior.random_number, lottery[0])
+
+            payoff = -999
+            # Payoff calculation must handle compound lotteries
+            if len(lottery) == 2:
+                # A simple lottery
+                payoff = lottery[1][outcome]
+            else:
+                # A compound lottery. Check the DoN indicator vector to see if the outcome has DoN
+                if lottery[2][outcome] == 1:
+                    if p_prior.random_coin == 'D':
+                        payoff = 2 * lottery[1][outcome]
+                    else:
+                        payoff = 0
+                else:
+                    payoff = lottery[1][outcome]
+
+            # using the built-in payoff field
+            player.participant.risk_payoff_round = p_prior.round_number
+            player.participant.risk_payoff = payoff
+            # The following line updates the otree built-in payment variable
+            player.payoff = payoff
+
+            # print(player.participant.vars)
+
+class Results(Page):
+    def is_displayed(player):
+        return player.subsession.round_number == player.subsession.session.vars['risk_last_round']
+
+    def vars_for_template(player: Player):
+        prior_round = player.participant.vars['risk_payoff_round']
+        p_prior = player.in_round(prior_round)
+
+        # Show the coin only if dealing with a compound lottery
+        show_coin = False
+        if p_prior.response == 'L':
+            selected_lottery = json.loads(p_prior.left_lottery)
+        else:
+            selected_lottery = json.loads(p_prior.right_lottery)
+        if len(selected_lottery) == 3:
+            # We have a compound lottery, so let's check if the outcome involved a DoN
+            outcome = check_partition(p_prior.random_number, selected_lottery[0])
+            if selected_lottery[2][outcome] == 1:
+                show_coin = True
+
+        return dict(
+            num_rounds=p_prior.subsession.session.vars['risk_last_round'],
+            qid=p_prior.qid,
+            left_lottery=json.loads(p_prior.left_lottery),
+            right_lottery=json.loads(p_prior.right_lottery),
+            random_number=p_prior.random_number,
+            random_coin=p_prior.random_coin,
+            show_random_coin=show_coin,
+            earnings=p_prior.participant.risk_payoff,
+            response=p_prior.response,
+        )
+
+
+
+
+page_sequence = [Instructions, MyPage, Results]

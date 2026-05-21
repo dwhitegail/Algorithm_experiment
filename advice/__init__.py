96e09d12b0980e09d4e2fa08bb10cef3e3fba726
Gail <dwhitegail@gmail.com>
2026-05-21 16:20:31 -0400

    added urn2

--- advice/__init__.py
+++ advice/__init__.py
@@ -12,7 +12,7 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'advice'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 5
+    NUM_ROUNDS = 6
     PARTICIPATION_FEE = 10.00
     ENDOWMENT = 10.00
 
